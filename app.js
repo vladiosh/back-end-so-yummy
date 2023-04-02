d068fae28e17b7a3219c2b399b481860eb3d026c
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-04-02 15:48:17 +0300

    add DB connection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+require("dotenv").config();
+
 const recipesRouter = require("./routes/api/recipes");
 
 const app = express();
@@ -11,6 +13,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/recipes", recipesRouter);
 
